f55886a60a3d911e971ce77f51ca2bf094197c55
bala-amypo <bala@amypo.in>
2025-12-17 09:46:01 +0000

    User commit

--- src/main/java/com/example/demo/controller/StudentController.java
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -1,15 +1,14 @@
-package com.example.demo.controller;
 
+package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.entity.Student;
 import com.example.demo.service.StudentService;
-
+import com.example.demo.entity.Student;
 @RestController
-public class StudentController {
+public class StudentController{
     @Autowired
     StudentService stdser;
     @PostMapping("/addStudent")
--- src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/StudentServiceimpl.java
@@ -1,20 +1,17 @@
+
 package com.example.demo.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.repository.StudentRepository;
 import org.springframework.stereotype.Service;
-
-import com.example.demo.service.Student;
-import com.example.demo.repositry.StudentRepository;
 import com.example.demo.service.StudentService;
-
-
+import com.example.demo.entity.Student;
+import org.springframework.beans.factory.annotation.Autowired;
 @Service
-public class StudentServiceImpl implements StudentService{
+public class StudentServiceImpl implements StudentService {
     @Autowired
     StudentRepository stdrepo;
     @Override
     public Student poststudent(Student st){
         return stdrepo.save(st);
-
     }
 }
